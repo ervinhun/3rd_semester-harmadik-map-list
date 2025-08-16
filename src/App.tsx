f4b87033a6c67edd5cdc6b4769169d574267818e
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-08-16 15:18:46 +0200

    person introduced

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
-
 function App() {
     const person = [
-        'Creola Katherine Johnson: mathematician',
-        'Mario José Molina-Pasquel Henríquez: chemist',
-        'Mohammad Abdus Salam: physicist',
-        'Percy Lavon Julian: chemist',
-        'Subrahmanyan Chandrasekhar: astrophysicist']
-    const listItems = person.map((person, index) => <li key={index}>{person}</li>)
+        {
+            id: 0,
+            name: 'Creola Katherine Johnson',
+            age: 31,
+            profession: 'mathematician'
+        },
+        {
+            id: 1,
+            name: 'Mario José Molina-Pasquel Henríquez',
+            age: 28,
+            profession: 'chemist'
+        },
+        {
+            id: 2,
+            name: 'Mohammad Abdus Salam',
+            age: 25,
+            profession: 'physicist'
+        },
+        {
+            id: 3,
+            name: 'Percy Lavon Julian',
+            age: 29,
+            profession: 'chemist'
+        },
+        {
+            id: 4,
+            name: 'Subrahmanyan Chandrasekhar',
+            age: 43,
+            profession: 'astrophysicist'
+        }]
+    const listItems = person.map((person) =>
+        <li key={person.id}>{person.name} ({person.age}): {person.profession}</li>)
+
+    const chemists = person.filter((person) => person.profession === 'chemist')
+    const listOfChemists = chemists.map((person) => <li key={person.id}>{person.name}</li>)
     return (
         <>
             <h1>List of people</h1>
             <ul>{listItems}</ul>
+            <br />
+            <h1>List of chemists</h1>
+            <ul>{listOfChemists}</ul>
         </>
     )
 }
